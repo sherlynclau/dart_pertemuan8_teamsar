389b8569a788cab65be2c963e19cc3c44e59a69c
AriadneClarissa <ariadneclarissadarnettaoswari_2327240056@mhs.mdp.ac.id>
2025-10-08 14:47:49 +0700

    Menambahkan Rumus Luas & Keliling Persegi Panjang

--- bin/dart_pertemuan8_teamsar.dart
+++ bin/dart_pertemuan8_teamsar.dart
@@ -3,4 +3,14 @@ class PersegiPanjang {
   int lebar;
 
   PersegiPanjang(this.panjang, this.lebar);
+
+  //Rumus Luas Persegi Panjang
+  int luasPersegiPanjang(){
+    return panjang*lebar;
+  }
+
+  //Rumus Keliling Persegi Panjang
+  int kelilingPersegiPanjang(){
+    return 2 * (panjang + lebar);
+  }
 }
